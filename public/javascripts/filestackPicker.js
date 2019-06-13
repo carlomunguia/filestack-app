6c47299512a8f2215eb0400cdd3a78094bde80f4
Carlo Munguia <carlomunguia@carlos-air.home>
2019-06-12 23:12:01 -0500

    implement image thumbnails & document previews

--- public/javascripts/filestackPicker.js
+++ public/javascripts/filestackPicker.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
-console.log("Hello from custom.js");
+console.log('Hello from custom.js');
 
 const client = filestack.init(filestackToken);
 const options = {
     onUploadDone: updateForm,
     maxSize: 10 * 1024 * 1024,
-    accept: 'image/*',
+    // accept: 'image/*',
     uploadInBackground: false
 };
 
@@ -14,20 +14,18 @@ const picker = client.picker(options);
 
 // get DOM elements
 
-const form = document.getElementById('form-pick');
-const fileInput = document.getElementById('fileupload');
+const form = document.getElementById('formPick');
+const fileInput = document.getElementById('fileUpload');
 const pickerBtn = document.getElementById('picker');
-const nameBox = document.getElementById('nameBox');
-const urlBox = document.getElementById('urlBox');
 const imageUrl = document.getElementById('imageURL');
-const thumbnail = document.getElementById('thumbnail');
+const container = document.getElementById('container')
 const thumbnailAnchor = document.getElementById('thumbnailAnchor');
 // add event listeners
 
 pickerBtn.addEventListener('click', function (e) {
     e.preventDefault();
     picker.open();
-    console.log("this is totally working");
+    console.log('this is totally working');
 });
 
 form.addEventListener('submit', function (e) {
@@ -37,17 +35,64 @@ form.addEventListener('submit', function (e) {
 
 // Helper to overwrite input value
 
-function updateForm (result) {
+function updateForm(result) {
+
     const fileData = result.filesUploaded[0];
     fileInput.value = fileData.url;
     imageUrl.value = fileInput.value;
-    const name = document.createTextNode('Selected: ' + fileData.filename);
+    createAnchor(fileData);
+
+    if (fileData.mimetype.indexOf('image/') === 0) {
+        createThumbnail(fileData)
+    } else {
+        createPreview(fileData);
+    }
+    console.log(fileData);
+}
+
+function getPreviewUrl(fileData) {
+    const urlComponents = fileData.url.split('/')
+    urlComponents.splice(3, 0, 'preview');
+    return urlComponents.join('/');
+}
+
+function createAnchor(fileData) {
+
+    const name = document.createElement('div');
+    name.textContent = 'Selected: ' + fileData.filename;
+    container.appendChild(name);
+
+    const div = document.createElement('div');
+    div.appendChild(document.createTextNode('Upload to: '));
+
+    const url = getAnchor(fileData);
+    url.textContent = fileData.url;
+    div.appendChild(url);
+    container.appendChild(div);
+}
+
+function createPreview(fileData) {
+    const iframe = document.createElement('iframe');
+    iframe.src = getPreviewUrl(fileData);
+    iframe.style.width = '100%';
+    iframe.style.height = '600px';
+    container.appendChild(iframe);
+}
+
+function createThumbnail(fileData) {
+    const anchor = getAnchor(fileData);
+
+    const image = document.createElement('img');
+    image.src = fileData.url;
+    image.style.maxWidth = '100%';
+    anchor.appendChild(image);
+    container.appendChild(anchor);
+}
+
+function getAnchor(fileData) {
+
     const url = document.createElement('a');
     url.href = fileData.url;
-    url.appendChild(document.createTextNode(fileData.url));
-    nameBox.appendChild(name);
-    urlBox.appendChild(document.createTextNode('Upload to: '));
-    urlBox.appendChild(url);
-    thumbnail.src = fileData.url;
-    thumbnailAnchor.href = fileData.url;
+    return url;
+
 }
